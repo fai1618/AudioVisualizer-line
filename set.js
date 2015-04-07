93f6aba35cc1182596179801db72d02bc9208310
fai1618 <fai1.618033@gmail.com>
2015-04-07 18:39:33 +0900

    周波数の大きさ？の変化を書き出した

--- set.js
+++ set.js
@@ -35,15 +35,19 @@ $(function(){
     fileReader.readAsArrayBuffer(e.target.files[0]);
   });
 　
-  render = function(){
-    var height = $(window).height();
-    var BorderWidth = analyser.frequencyBinCount * 10;
-    var a = 0.5;
-    var _r = '00';
-    var _g = '88';
-    var _b = 'c1';
+  var difference = 0;
+  var Ava = 0;
+  var preAva = 0;
+  var height = $(window).height();
+  var BorderWidth = analyser.frequencyBinCount * 10;
+  var a  = 0.5;
+  var _r = '00';
+  var _g = '88';
+  var _b = 'c1';
 
-    var spectrums = new Uint8Array(analyser.frequencyBinCount);//周波数
+  render = function(){
+
+    var spectrums = new Uint8Array(analyser.frequencyBinCount);
     //var spectrums = new Float32Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(spectrums);
     //analyser.getFloatFrequencyData(spectrums);
@@ -51,25 +55,35 @@ $(function(){
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     
     for(var i=0, len=spectrums.length; i<len; i++){
+      Ava += spectrums[i];
+
       if( 85 > spectrums[i] ){
         //canvasContext.fillStyle = 'rgba(255,0,0,'+a+')';
         canvasContext.fillStyle = '#'+_b+_r+_g;
       }else{
-        if( 85 <= spectrums[i] && spectrums[i] < 170){
-          //canvasContext.fillStyle = 'rgba(0,255,0,'+a+')';
-          canvasContext.fillStyle = '#'+_g+_b+_r;
-        }else{
-          if( spectrums[i] >= 170 ){
-            //canvasContext.fillStyle = 'rgba(0,0,255,'+a+')';
-            canvasContext.fillStyle = '#'+_r+_g+_b;
+          if( 85 <= spectrums[i] && spectrums[i] < 170){
+            //canvasContext.fillStyle = 'rgba(0,255,0,'+a+')';
+            canvasContext.fillStyle = '#'+_g+_b+_r;
+          }else{
+            if( spectrums[i] >= 170 ){
+              //canvasContext.fillStyle = 'rgba(0,0,255,'+a+')';
+              canvasContext.fillStyle = '#'+_r+_g+_b;
+            }
           }
-        }
       }
       var BarHeight = spectrums[i]/2;
       var x = i*2;
       canvasContext.fillRect(x, height-BarHeight-300, 1, BarHeight);
       canvasContext.fillRect(x, height-300, 1, BarHeight);
     }
+
+    Ava   = Ava/(spectrums.length-1);// Ava/i と同じ
+    difference = Ava - preAva;
+    preAva = Ava;
+
+    $("#difference").text("diffrrence: "+difference);
+    $("#Ava").text("Ava: "+Ava);
+
     animationId = requestAnimationFrame(render);
   };
 });
